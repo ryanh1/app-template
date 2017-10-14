8c73f96b8dab7d288c6763dc11291be673e94ad1
Ryan Hollander <ryanhollander@DN0a632b78.SUNet>
2017-10-14 02:57:26 -0700

    Add validation for user already in the database

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -6,34 +6,12 @@ var LocalStrategy = require('passport-local').Strategy;
 // Require self-made files
 var User = require('../models/user');
 
-
-// Passport authentication middlewear
-passport.use(new LocalStrategy({
-  usernameField: 'email'
-},
-  function(username, password, done) {
-   User.getUserByEmail(username, function(err, user){
-   	if(err) throw err;
-   	if(!user){
-   		return done(null, false, {message: 'Unknown User'});
-   	}
-
-   	User.comparePassword(password, user.password, function(err, isMatch){
-   		if(err) throw err;
-   		if(isMatch){
-   			return done(null, user);
-   		} else {
-   			return done(null, false, {message: 'Invalid password'});
-   		}
-   	});
-  });
-}));
-
+// Strategy for signup
 // Passport sign up middlewear
 passport.use('local-signup', new LocalStrategy({
     usernameField: 'email',
     passReqToCallback: true
-  },  function(req, username, done) {
+  },  function(req, username, password, done) {
 
           // Sign up user
         	var name = req.body.name;
@@ -46,7 +24,6 @@ passport.use('local-signup', new LocalStrategy({
           // Then it posts errors in req.validationErrors
 
         	req.checkBody('name', 'Name is required').isLength({ min: 1});
-          req.checkBody('name', 'Name is invalid').check);
         	req.checkBody('email', 'Email is required').isLength({ min: 1});
         	req.checkBody('email', 'Email is not valid').isEmail();
         	req.checkBody('password', 'Password is required').notEmpty();
@@ -61,17 +38,25 @@ passport.use('local-signup', new LocalStrategy({
           		 	errors:errors
           		});
         	} else {
+
+            // Else, check for that user in the database
+            User.getUserByEmail(email, function(err, user){
+              if(err) throw err;
+              if(user) {
+                return done(null, false, {message: 'A user with that email already exists'});
+              }
+
               // Else, create a new user
-          		var newUser = new User({
-          			email:email,
-          			password: password,
+              var newUser = new User({
+                email: email,
+                password: password,
                 name: name
-          		});
+              });
 
               // Save the user to the database
-          		User.createUser(newUser, function(err, user){
-          			if(err) throw err;
-          			console.log(user);
+              User.createUser(newUser, function(err, user){
+                if(err) throw err;
+                console.log(user);
                 console.log('Saved user to database');
 
                 User.getUserByEmail(email, function(err, user){
@@ -91,6 +76,8 @@ passport.use('local-signup', new LocalStrategy({
                     });
                 });
               });
+            });
+
       		}
           // Send the positive flash message
       		// req.flash('success_msg', 'You are registered and can now login');
@@ -99,6 +86,29 @@ passport.use('local-signup', new LocalStrategy({
           // res.redirect('profile');
     }
 ));
+
+
+// Strategy for login
+passport.use('local', new LocalStrategy({
+    usernameField: 'email'
+  },
+  function(username, password, done) {
+   User.getUserByEmail(username, function(err, user){
+   	if(err) throw err;
+   	if(!user){
+   		return done(null, false, {message: 'Unknown User'});
+   	}
+
+   	User.comparePassword(password, user.password, function(err, isMatch){
+   		if(err) throw err;
+   		if(isMatch){
+   			return done(null, user);
+   		} else {
+   			return done(null, false, {message: 'Invalid password'});
+   		}
+   	});
+   });
+}));
 
 
 passport.serializeUser(function(user, done) {
@@ -110,6 +120,5 @@ passport.deserializeUser(function(id, done) {
     done(err, user);
   });
 });
-
 
 module.exports = {passport};
